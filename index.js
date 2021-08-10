159a4976c80e8395f3a89d1937c57af9d731f1a8
Thomas <thomasmckenna12gmail.com>
2021-08-10 16:27:42 -0400

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 2; 
+let num2 = 31; 
+let multiply = num1 * num2; 
+multiply; 
+
+
+let random = Math.floor(Math.random() * 15) + 1; 
+
+
+let num3 = 19
+let num4 = 5
+let mod =  num3 % num4; 
+
+mod; 
+
+
+let max = Math.max(5, 10, 15, 20);
+
